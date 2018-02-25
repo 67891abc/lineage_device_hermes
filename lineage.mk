6cc908433da7e7599a148a5495997938dffedf7d
67891abc <1768300180abc@gmail.com>
2018-02-25 17:45:47 +0800

    hermes: re-enable vendor/lineage

--- lineage.mk
+++ lineage.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
 # Inherit some common Lineage stuff.
-#$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device display
 TARGET_SCREEN_HEIGHT := 1920
